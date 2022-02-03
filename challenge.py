e41993e0d7b0f6f3225839e7cbc0bb4fb077f974
Daniela <danimaltoc@gmail.com>
2022-02-02 22:29:41 -0300

    frist-commit

---
+++ challenge.py
@@ -0,0 +1,20 @@
+count = 0
+opcao = ""
+
+from random import randint
+from time import sleep
+while opcao !="n":
+    computer = randint(10,10)
+    print('-=-' * 20)
+    print('VOU PENSAR EM UM NUMERO ENTRE 10 e 1000. TENTE ADIVINHAR...')
+    print('-=-' * 20)
+    jogador = int(input("EM QUE NUMERO EU PENSEI?"))
+    print("PROCESSANDO...")
+    sleep(2)
+    if jogador == computer:
+        print("UAUUUUUUUUUU VOCE CONSEGUIU ME VENCER!")
+    else:
+        print(f"GANHEI! PENSEI NO NUMERO {computer} E NAO NO {jogador}!")
+    opcao = str(input("VOCÊ DESEJA TENTAR OUTRA VEZ [s/n]?"))
+
+
