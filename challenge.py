db5a6867092c4dbba985336e7f86701f3dc72d20
Daniela <danimaltoc@outlook.com>
2023-01-26 14:33:58 -0300

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -1,17 +1,38 @@
-count = 0
-option = ""
-from random import randint
 from time import sleep
-while option !="n":
-    computer = randint(10,1000)
-    print('-=-' * 20)
-    print('VOU PENSAR EM UM NÚMERO ENTRE 10 E 1000. TENTE ADIVINHAR....')
-    print('-=-' * 20)
-    jogador = int(input("EM QUE NÚMERO EU PENSEI?"))
+from random import randrange
+rodadas = 1
+chances = 0
+secret = randrange(10,1000)
+
+print("QUAL É O NÍVEL DE DIFICULDADE?")
+print("[1] FÁCIL")
+print("[2] MEDIA")
+print("[3] DIFÍCIL")
+nivel = int(input("DEFINA O NÍVEL:"))
+niveis = int(nivel)
+if (nivel == 1):
+    chances = 20
+elif (nivel == 2):
+    chances = 10
+else:
+    chances = 5
+for rodadas in range(1, chances + 1):
+    print("TENTATIVAS: {} de {}".format(rodadas,chances))
+    chutes = str(input("INFORME UM NUMERO ENTRE 1 E 1000:")).strip()
+    chute = int(chutes)
+    if (chute < 10 or chute > 1000):
+        print("VOCÊ DEVE DIGITAR UM NÚMERO ENTRE 10 E 1000!")
+        continue
     print("PROCESSING...")
-    sleep(2)
-    if jogador == computer:
-        print("WOOOOOOOOOOOOOOOO VOCÊ ME FEZ!")
+    sleep(3)
+    if (secret == chute):
+        print("VOCÊ ACERTOU")
+        break
     else:
-        print(f"WON! EU PENSEI NO NÚMERO {computer} E NÃO{jogador}!")
-    opcao = str(input("VOCÊ QUER TENTAR DE NOVO [S/N] ?"))
+        if (chute > secret):
+                print("VOCE ERRROUUU! O SEU CHUTE FOI MAIOR.")
+        elif (chute < secret):
+            print("VOCE ERRROUUU ! SEU CHUTE FOI MENOR .")
+            rodadas = rodadas + 1
+
+print("GAME OVER")
